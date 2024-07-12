56cbb9f276ed9340725fe449ff069550d8fdc940
leejinwoo <leejinwoo1126@gmail.com>
2024-07-12 11:21:12 +0900

    refactor: 클래스 분리, 의존성 주입

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public abstract class PaymentService {
+public class PaymentService {
+    private final WebApiExRateProvider webApiExRateProvider;
+
+    public PaymentService(WebApiExRateProvider webApiExRateProvider) {
+        this.webApiExRateProvider = webApiExRateProvider;
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+        BigDecimal exRate = webApiExRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
 }
--- src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
@@ -10,9 +10,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.stream.Collectors;
 
-public class WebApiExRatePaymentService extends PaymentService {
+public class WebApiExRateProvider {
 
-    @Override
     BigDecimal getExRate(String currency) throws IOException {
         URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -22,7 +21,7 @@ public class WebApiExRatePaymentService extends PaymentService {
 
         ObjectMapper mapper = new ObjectMapper();
         ExRateData exRateData = mapper.readValue(response, ExRateData.class);
-        BigDecimal exRate = exRateData.rates().get("KRW");
-        return exRate;
+
+        return exRateData.rates().get("KRW");
     }
 }
