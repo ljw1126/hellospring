e505cb76b107f5dd59778d9ed240be027192068d
leejinwoo <leejinwoo1126@gmail.com>
2024-07-12 10:30:02 +0900

    feat: PaymentService 개발(2)

--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,4 +1,58 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    private Long orderId;
+    private String currency; // 종류
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount; // 환산 금액
+    private LocalDateTime validUntil; //환율 유효 기간
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
