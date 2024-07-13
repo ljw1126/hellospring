8f78b304a4f3a5617d6197bd7b485569fb4189cc
leejinwoo <leejinwoo1126@gmail.com>
2024-07-13 10:03:36 +0900

    refactor: WebApiExRateProvider throws 제거, try-catch 처리

--- src/main/java/tobyspring/hellospring/exrate/SimpleExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/SimpleExRateProvider.java
@@ -2,13 +2,12 @@ package tobyspring.hellospring.exrate;
 
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
 public class SimpleExRateProvider implements ExRateProvider {
 
     @Override
-    public BigDecimal getExRate(String currency) throws IOException {
+    public BigDecimal getExRate(String currency) {
         if ("USD".equals(currency)) return BigDecimal.valueOf(100);
 
         throw new IllegalArgumentException("지원되지 않는 통화입니다");
