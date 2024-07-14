a5eefad5af9e5081bffc2051f581fdba281b0426
leejinwoo <leejinwoo1126@gmail.com>
2024-07-14 13:22:05 +0900

    refactor: 기술에 독립적인 애플리케이션 서비스

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -1,23 +1,11 @@
 package tobyspring.hellospring.order;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
 import java.math.BigDecimal;
 
-@Entity
-@Table(name = "orders")
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String no;
 
     private BigDecimal total;
